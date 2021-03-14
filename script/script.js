ac8a513e4aece00eb60616b972c0425c0431dff4
Nikita <Kent720p@gmail.com>
2021-03-14 12:17:31 +0300

    creating mobile layout

--- script/script.js
+++ script/script.js
@@ -1,12 +1,34 @@
 if (window.matchMedia("(max-width: 490px)").matches) {
-    let currentSlide = 0;
-    const reviews = document.querySelectorAll('.review');
-    console.log(reviews)
-    reviews.forEach((review, i)=>{
-        if(i != currentSlide) {
-            review.classList.add('hide');
-        }
-    })
+    let slideIndex = 1;
+    showSlide(slideIndex)
+
 
 } else {
 }
+
+function showSlide(currentSlide) {
+
+    const reviews = document.querySelectorAll('.review');
+
+    if(currentSlide > reviews.length) {
+        slideIndex = 1;
+    }
+    if(currentSlide < 1) {
+        slideIndex = reviews.length;
+    }
+    reviews.forEach((review, i)=>{
+            review.style.display = 'none';
+    })
+    reviews[slideIndex-1].style.display = "block";
+}
+
+function nextSlide(slide) {
+    showSlides(slideIndex += 1);
+}
+function prevSlide(slide) {
+    showSlides(slideIndex -= 1);
+}
+
+// function curSlide(slide) {
+//     showSlides(slideIndex = n);
+// }
