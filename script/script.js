dbab6e10cc9b6fa620588337a3eea213694dafa1
Nikita <Kent720p@gmail.com>
2021-03-14 15:55:28 +0300

    responsive mobile footer

--- script/script.js
+++ script/script.js
@@ -1,34 +1,53 @@
 if (window.matchMedia("(max-width: 490px)").matches) {
+      footerWrapper();  
     let slideIndex = 1;
     showSlide(slideIndex)
+    let review = document.querySelector('.reviews-wrap');
+
+    review.insertAdjacentHTML("beforeend",
+        `<span class="slider-toggler-right review-next"                          onclick="nextReview()"></span>`);
 
 
-} else {
+
+
 }
+
 
 function showSlide(currentSlide) {
-
     const reviews = document.querySelectorAll('.review');
+    if (currentSlide !== 1) {
+        let review = document.querySelector('.reviews-wrap');
 
-    if(currentSlide > reviews.length) {
+        review.insertAdjacentHTML("beforeend",
+            `<span class="slider-toggler-left review-prev"                          onclick="prevReview()"></span>`);
+
+
+
+
+
+    } else {
+
+    }
+
+    if (currentSlide > reviews.length) {
         slideIndex = 1;
+
+
     }
-    if(currentSlide < 1) {
+    if (currentSlide < 1) {
         slideIndex = reviews.length;
+
     }
-    reviews.forEach((review, i)=>{
-            review.style.display = 'none';
+    reviews.forEach((review, i) => {
+        review.style.display = 'none';
     })
-    reviews[slideIndex-1].style.display = "block";
+    reviews[currentSlide - 1].style.display = "block";
 }
 
-function nextSlide(slide) {
-    showSlides(slideIndex += 1);
-}
-function prevSlide(slide) {
-    showSlides(slideIndex -= 1);
+function nextReview() {
+    showSlide(slideIndex += 1);
 }
 
-// function curSlide(slide) {
-//     showSlides(slideIndex = n);
-// }
+function prevReview() {
+    showSlide(slideIndex -= 1);
+}
